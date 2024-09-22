872a0eca5229d633bf211d6d36338ff7b77cb60b
Souvik Bhattacharyya <souvikshanku@gmail.com>
2024-09-22 11:55:39 +0530

    dataloader

---
+++ dataset.py
@@ -0,0 +1,42 @@
+import os
+
+import pandas as pd
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+
+class MultiMnist(Dataset):
+    def __init__(self, annotations_file, img_dir, device):
+        self.img_labels = pd.read_csv(annotations_file, header=None)
+        self.num_classes = 11
+        self.end_token = 10
+        self.seq_len = 3
+
+        self.img_dir = img_dir
+        self.device = device
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+
+        label = self.img_labels.iloc[idx, 1]
+        label = self.one_hot_encode(label)
+
+        image = image.to(self.device)
+        label = label.to(self.device)
+
+        return image, label
+
+    def one_hot_encode(self, label):
+        # 12 -> [1, 2]
+        tokens = list(map(int, str(label)))
+        # [1, 2] -> [1, 2, <end>]
+        tokens += [self.end_token] * (self.seq_len - len(tokens))
+
+        y = F.one_hot(torch.tensor(tokens), self.num_classes)
+        return y
